1da955c73f408369829ef14fd0597c3f61eb4859
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2022-12-04 22:27:56 +0530

    added common models for user

---
+++ src/main/java/com/core/libraries/common/models/user/UserData.java
@@ -0,0 +1,15 @@
+package com.core.libraries.common.models.user;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserData {
+    private String firstName;
+    private String lastName;
+    private String number;
+    private String email;
+}
